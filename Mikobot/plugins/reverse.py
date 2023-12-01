52649aaca9c35811d091d707b6a780902cb03630
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2023-12-01 20:00:20 +0600

    correction

--- Mikobot/plugins/reverse.py
+++ Mikobot/plugins/reverse.py
@@ -42,15 +42,16 @@ async def reverseImageSearch(_: Client, m: t.Message):
         os.remove(file)
         if imgUrl is None:
             return await reply.edit("Ran into an error.")
-        output = await ReverseImageSearch("google", f"https://graph.org/{imgUrl[0]}")
+        output = await reverse_image_search("google", f"https://graph.org/{imgUrl[0]}")
         if output is None:
             return await reply.edit("Ran into an error.")
-        message = ""
+
         names = output["content"]["bestResults"]["names"]
         urls = output["content"]["bestResults"]["urls"]
         btn = t.InlineKeyboardMarkup(
             [[t.InlineKeyboardButton(text="IMAGE URL", url=urls[-1])]]
         )
+
         if len(names) > 10:
             message = "\n".join(
                 [f"{index+1}. {name}" for index, name in enumerate(names[:10])]
@@ -63,16 +64,14 @@ async def reverseImageSearch(_: Client, m: t.Message):
             htmlMessage += (
                 "<br/><br/>By <a href='https://lexica.qewertyy.me'>LexicaAPI</a>"
             )
-            url = telegraph.create_page("More Results", html_content=htmlMessage)
-            message += f"\n\n[More Results]({url})\nBy @LexicaAPI"
+            telegraph_page = telegraph.create_page(
+                "More Results", html_content=htmlMessage
+            )
+            message += f"\n\n[More Results](https://telegra.ph/{telegraph_page['path']})\n\nBy @LexicaAPI"
             await reply.delete()
             return await m.reply_text(message, reply_markup=btn)
-        message = "\n".join(
-            [
-                f"{index+1}. {name}"
-                for index, name in enumerate(output["content"]["bestResults"]["names"])
-            ]
-        )
+
+        message = "\n".join([f"{index+1}. {name}" for index, name in enumerate(names)])
         await reply.delete()
         await m.reply_text(f"{message}\n\nBy @LexicaAPI", reply_markup=btn)
     except Exception as E:
@@ -80,7 +79,7 @@ async def reverseImageSearch(_: Client, m: t.Message):
         return await m.reply_text("Ran into an error.")
 
 
-async def ReverseImageSearch(search_engine, img_url) -> dict:
+async def reverse_image_search(search_engine, img_url) -> dict:
     try:
         response = await state.post(
             f"https://lexica.qewertyy.me/image-reverse/{search_engine}?img_url={img_url}",
