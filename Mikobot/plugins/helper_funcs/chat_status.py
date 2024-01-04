19f550db7a77580104ba0dc3aab3759a50b8e123
Karma <116187035+Infamous-Hydra@users.noreply.github.com>
2024-01-04 21:18:39 +0600

    Update chat_status.py

--- Mikobot/plugins/helper_funcs/chat_status.py
+++ Mikobot/plugins/helper_funcs/chat_status.py
@@ -194,6 +194,10 @@ def is_support_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool
     return user_id in DRAGONS or user_id in DEV_USERS
 
 
+def is_sudo_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
+    return user_id in DRAGONS or user_id in DEV_USERS
+
+
 async def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
         chat.type == "private"
@@ -259,6 +263,58 @@ async def is_user_ban_protected(
 async def is_user_in_chat(chat: Chat, user_id: int) -> bool:
     member = await chat.get_member(user_id)
     return member.status in (ChatMemberStatus.LEFT, ChatMemberStatus.RESTRICTED)
+
+
+def dev_plus(func):
+    @wraps(func)
+    def is_dev_plus_func(
+        update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+    ):
+        context.bot
+        user = update.effective_user
+
+        if user.id in DEV_USERS:
+            return func(update, context, *args, **kwargs)
+        elif not user:
+            pass
+        elif DEL_CMDS and " " not in update.effective_message.text:
+            try:
+                update.effective_message.delete()
+            except:
+                pass
+        else:
+            update.effective_message.reply_text(
+                "This is a developer restricted command."
+                " You do not have permissions to run this."
+            )
+
+    return is_dev_plus_func
+
+
+def sudo_plus(func):
+    @wraps(func)
+    def is_sudo_plus_func(
+        update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+    ):
+        context.bot
+        user = update.effective_user
+        chat = update.effective_chat
+
+        if user and is_sudo_plus(chat, user.id):
+            return func(update, context, *args, **kwargs)
+        elif not user:
+            pass
+        elif DEL_CMDS and " " not in update.effective_message.text:
+            try:
+                update.effective_message.delete()
+            except:
+                pass
+        else:
+            update.effective_message.reply_text(
+                "Who dis non-admin telling me what to do? You want a punch?"
+            )
+
+    return is_sudo_plus_func
 
 
 def support_plus(func):
